0fcebccd9c1bbbc538d99e8ac34d1feeb8903c5c
RichT <rrtrenchf@gmail.com>
2020-07-24 13:19:12 -0400

    styled dashboard, search on resources page, api linked to button on resource page

--- public/js/api.js
+++ public/js/api.js
@@ -1,12 +1,22 @@
 //Prorepublica bill search for coronavirus
-$.ajax({
-    url:"https://api.propublica.org/congress/v1/bills/search.json?query=coronavirus-housing",
-    method:"GET",
-    dataType: "json",
-    headers:{'X-API-Key':'D4baiyH8PfzjJXFGBVMa5eWkcf6CtQwOIexmhBkm'}
-}).then(function(data){
-console.log(data)
-}).catch(function(err){
-    throw err
-})
+$(function () {
+    $("button").on("click", function (event) {
+
+        var billSearch = $("#billSearch").val()
+        console.log(billSearch)
+    });
+    // Send the PUT request.
+    $.ajax({
+        url: "https://api.propublica.org/congress/v1/bills/search.json?query=" + billSearch,
+        method: "GET",
+        dataType: "json",
+        headers: { 'X-API-Key': 'D4baiyH8PfzjJXFGBVMa5eWkcf6CtQwOIexmhBkm' }
+    }).then(function (data) {
+        console.log(data)
+    }).catch(function (err) {
+        throw err
+    })
+});
+
+
 
