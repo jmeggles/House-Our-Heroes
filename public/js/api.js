120de200f48a8f98f679ed01d4eca4a80d71fb9b
RichT <rrtrenchf@gmail.com>
2020-07-24 14:37:57 -0400

    trying to get title and summary to show up in results div

--- public/js/api.js
+++ public/js/api.js
@@ -5,17 +5,24 @@ $(function () {
         var billSearch = $("#billSearch").val()
         console.log(billSearch)
     });
-    // Send the PUT request.
+    //call to api.
     $.ajax({
         url: "https://api.propublica.org/congress/v1/bills/search.json?query=" + billSearch,
         method: "GET",
         dataType: "json",
         headers: { 'X-API-Key': 'D4baiyH8PfzjJXFGBVMa5eWkcf6CtQwOIexmhBkm' }
-    }).then(function (data) {
-        console.log(data)
-    }).catch(function (err) {
-        throw err
+    }).then(function (response) {
+        // console.log(response.results[0].bills[i].title)
+        console.log(response)
+        
+        const billArray = []
+        for (var i = 0; i<response.results.title.length; i++){
+            
+
+        }
     })
+   
+    
 });
 
 
