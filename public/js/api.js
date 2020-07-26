01c812f56fd692b9fe3e6ef41f881d440277498a
RichT <rrtrenchf@gmail.com>
2020-07-26 16:34:38 -0400

    search works. only needs styling

--- public/js/api.js
+++ public/js/api.js
@@ -18,14 +18,18 @@ $(function () {
             // const billArray = []
             for (var i = 0; i < response.results[0].bills.length; i++) {
 
-                title = response.results[0].bills[i].title
-                summary = response.results[0].bills[i].summary
-                console.log(title)
-                // console.log(summary)
-                titleResult = $('.result').text(title)
-
-
-            }
+                // gather the response
+                var title = response.results[0].bills[i].title;
+              
+                // create a child container and set the text
+                var newDiv = $("<div>");
+                newDiv.text(title)
+              
+                // append this new child container to the parent container
+                $(".result").append(newDiv)
+              
+              }
+            
             
 
 
