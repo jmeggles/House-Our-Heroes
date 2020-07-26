89823d653035951a280464becca294c20de3480a
RichT <rrtrenchf@gmail.com>
2020-07-26 13:55:51 -0400

    Response shows tite and summary

--- public/js/api.js
+++ public/js/api.js
@@ -1,29 +1,36 @@
 //Prorepublica bill search for coronavirus
 $(function () {
     $("button").on("click", function (event) {
+        event.preventDefault();
 
         var billSearch = $("#billSearch").val()
         console.log(billSearch)
-    });
-    //call to api.
-    $.ajax({
-        url: "https://api.propublica.org/congress/v1/bills/search.json?query=" + billSearch,
-        method: "GET",
-        dataType: "json",
-        headers: { 'X-API-Key': 'D4baiyH8PfzjJXFGBVMa5eWkcf6CtQwOIexmhBkm' }
-    }).then(function (response) {
-        // console.log(response.results[0].bills[i].title)
-        console.log(response)
-        
-        const billArray = []
-        for (var i = 0; i<response.results.title.length; i++){
-            
 
-        }
-    })
-   
-    
+        //call to api.
+        $.ajax({
+            url: "https://api.propublica.org/congress/v1/bills/search.json?query=" + billSearch,
+            dataType: "json",
+            headers: { 'X-API-Key': 'D4baiyH8PfzjJXFGBVMa5eWkcf6CtQwOIexmhBkm' }
+        }).then(function (response) {
+
+
+            // const billArray = []
+            for (var i = 0; i < response.results[0].bills.length; i++) {
+
+                title = response.results[0].bills[i].title
+                summary = response.results[0].bills[i].summary
+                console.log(title)
+                console.log(summary)
+
+
+            }
+
+
+        })
+    });
+
 });
+
 
 
 
