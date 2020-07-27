128f830b2ea3d1fc6dd1202101a533c827a6a460
Jody Eggleston <jmeggles@gmail.com>
2020-07-27 12:25:00 -0400

    added table to resources page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const apiRoutes = require("./apiRoutes");
 const htmlRoutes = require("./htmlRoutes");
 const authRoutes = require("./authRoutes");
 
+// using this path inside all routes
 //auth routes
 router.use("/auth", authRoutes);
 
