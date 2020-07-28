8ddd515dee258b22a0e65cb79fce8c0af2d506b3
RichT <rrtrenchf@gmail.com>
2020-07-27 21:20:22 -0400

    added rss to resources and modified news page buttons needs to be aligned

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 // *********************************************************************************
 // api-routes.js - this file offers a set of routes for displaying and saving data to the db
 // *********************************************************************************
